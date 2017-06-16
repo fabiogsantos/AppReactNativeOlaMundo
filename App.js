cda8daf3e8db163c16902317a149d7ad22e05cb5
fabiogsantos <fabiogsantos@gmail.com>
2017-06-16 01:37:11 -0300

    Cores

--- App.js
+++ App.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MyComponent from './components/MyComponent';
 
 export default class App extends React.Component {
+  state = {
+    valorInicial: 1
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Olá Mundo!</Text>
-        <Text>Fabio Gonçalves</Text>
-      </View>
+       <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+         {/*Vermelhor*/}
+         <View style={{flex:1, flexDirection:'column', backgroundColor:'#FF0000', justifyContent:'flex-start', alignItems:'flex-start'}}/>
+         {/*// Verde*/}
+         <View style={{flex:1, flexDirection:'column', backgroundColor:'#00FF00', justifyContent:'flex-start', alignItems:'flex-end'}}/>
+         {/*// Amarelo*/}
+         <View style={{flex:1, flexDirection:'column', backgroundColor:'#FFFF00', justifyContent:'flex-end', alignItems:'flex-end'}}/>
+         {/*// Azul*/}
+         <View style={{flex:1, flexDirection:'column', backgroundColor:'#0101DF', justifyContent:'flex-end', alignItems:'flex-start'}}/>
+         <View style={{flex:15, justifyContent:'center', alignItems:'center'}}>
+           <Text>
+             Olá React Native
+           </Text>
+         </View>
+         <View style={{flex:15, justifyContent:'center', alignItems:'center'}}>
+           <Button title="Definir Valor" onPress={()=>this.setState({valorInicial: 2})}></Button>
+           <MyComponent nome="React" inicial={this.state.valorInicial}/>
+         </View>  
+       </View>
     );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#d5d594',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  };
+};
